5c57ac5e2585a3e14882b637f13d90bf54b63362
Enrique Collado <enriquecollado@MacBook-Air-de-Enrique.local>
2026-04-16 09:09:30 +0200

    fix: app.py sin imports de main

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def webhook():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
