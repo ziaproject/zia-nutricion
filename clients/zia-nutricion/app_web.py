3ec803d31325dc40d343b35bf59126d85eabda62
Enrique Collado <enriquecollado@MacBook-Air-de-Enrique.local>
2026-05-10 15:38:34 +0200

    fix: CORS zianutricion.com

--- clients/zia-nutricion/app_web.py
+++ clients/zia-nutricion/app_web.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from supabase import create_client
 app = Flask(__name__)
+CORS(app, origins=["https://zianutricion.com", "https://www.zianutricion.com", "*"])
 CORS(app, origins=["https://zianutricion.com","https://www.zianutricion.com","http://localhost:5001"])
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
